92cd9c9efc8cd1ffda6692657811d8c684853ae1
Michael Broadbent~ <michael.broadbent95@gmail.com>
2024-05-15 15:34:59 +0100

    styling continued

--- scripts/app.js
+++ scripts/app.js
@@ -1,55 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-  //   const prevButton = document.querySelector(".prev-button");
-  //   const nextButton = document.querySelector(".next-button");
-  //   const carouselInner = document.querySelector(".carousel-inner");
-  //   const carouselItems = document.querySelectorAll(".carousel-item");
-  //   let currentIndex = 0;
-
-  //   function slide(direction) {
-  //     const itemWidth = carouselInner.children[0].offsetWidth;
-  //     currentIndex = (currentIndex + direction) % carouselInner.children.length;
-  //     carouselInner.style.transform = `translateX(-${
-  //       currentIndex * itemWidth
-  //     }px)`;
-  //   }
-
-  //   function showCurrentItem() {
-  //     carouselItems.forEach((item, index) => {
-  //       if (index === currentItemIndex) {
-  //         item.style.display = "block";
-  //       } else {
-  //         item.style.display = "none";
-  //       }
-  //     });
-  //   }
-
-  //   function showNextItem() {
-  //     currentItemIndex = (currentItemIndex + 1) % carouselItems.length;
-  //     showCurrentItem();
-  //   }
-
-  //   function showPrevItem() {
-  //     currentItemIndex =
-  //       (currentItemIndex - 1 + carouselItems.length) % carouselItems.length;
-  //     showCurrentItem();
-  //   }
-
-  //   //   prevButton.addEventListener("click", () => slide(-1));
-  //   //   nextButton.addEventListener("click", () => slide(1));
-
-  //   prevButton.addEventListener("click", showPrevItem);
-  //   nextButton.addEventListener("click", showNextItem);
-
-  //   carouselItems.forEach((item) => {
-  //     item.addEventListener("click", function (event) {
-  //       event.stopPropagation(); // Prevents the carousel from sliding when clicking the item
-  //       const link = this.getAttribute("href");
-  //       window.open(link, "_blank");
-  //     });
-  //   });
-
-  //   showCurrentItem();
-
   const carouselItems = document.querySelectorAll(".carousel-item");
   const prevButton = document.querySelector(".prev-button");
   const nextButton = document.querySelector(".next-button");
@@ -81,6 +30,5 @@ document.addEventListener("DOMContentLoaded", function () {
   prevButton.addEventListener("click", showPrevItem);
   nextButton.addEventListener("click", showNextItem);
 
-  // Initially show the first item
   showCurrentItem();
 });
